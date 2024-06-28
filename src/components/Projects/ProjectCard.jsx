110f42aebb9863d1c9389cfd7e8c8242fb0c478b
TannerM03 <tanner.macpherson3@gmail.com>
2024-06-28 17:07:20 -0400

    add project images, alter projectCard UI

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -8,7 +8,11 @@ export const ProjectCard = ({ project: {title, imageSrc, description, skills, de
     <div className = {styles.container}>
         <img className = {styles.image} src={getImageUrl(imageSrc)} alt={`Image of ${title}`} />
         <h3 className = {styles.title} >{title}</h3>
-        <p className = {styles.description} >{description}</p>
+        <ul className={styles.description}>
+        {description.map((item, id) => (
+          <li key={id}>{item}</li>
+        ))}
+      </ul>
         <ul className = {styles.skills} >{skills.map((skill, id) => {
             return (<li className = {styles.skill} key = {id}>{skill}</li>);
         })}
